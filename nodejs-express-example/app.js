ca520ac9d7442092b19a96d15b91d89f477e1f5d
Fulvio <fulvio.pascotto@gmail.com>
2020-09-22 18:37:19 +0200

    Refactoring nodejs app and addedd getUserByIdApi

--- nodejs-express-example/app.js
+++ nodejs-express-example/app.js
@@ -2,22 +2,28 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-
-var swaggerUi = require('swagger-ui-express'),
-    swaggerDocument = require('./swagger.json');
+var swaggerUi = require('swagger-ui-express');
+var swaggerDocument = require('./swagger.json');
 
 const BASE_PATH = '/api/v1';
 
-
 var app = express();
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+//Add logging fwk
+app.use(logger(':method :url :status :res[content-length] - :response-time ms'));
 
+//Add swagger doc
+app.use('/docs', function(req, res, next){
+  swaggerDocument.host = req.get('host');
+  req.swaggerDoc = swaggerDocument;
+  next();
+}, swaggerUi.serve, swaggerUi.setup());
+
+//Add Apis routes
 app.use(BASE_PATH + '/users', require('./routes/users'));
 app.use(BASE_PATH + '/system', require('./routes/system'));
 
-
-app.use(logger('dev'));
+//Configure express json and urlencoding
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -36,5 +42,33 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+var port = normalizePort(process.env.PORT || '3000');
+
+
+app.listen(port, () => {
+  console.debug('App listening on :'+ port);
+});
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
 
 module.exports = app;
--- nodejs-express-example/routes/users.js
+++ nodejs-express-example/routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+//Get MongoClient
 const { MongoClient } = require("mongodb");
 
-
+//Get connection variables
 const MONGO_URI = process.env.MONGO_URI;
 const MONGO_DB = process.env.MONGO_DB;
 const MONGO_COLLECTION = "user";
@@ -18,6 +19,18 @@ router.get('/', async function(req, res, next) {
   
 });
 
+/* GET user detail. */
+router.get('/:id', async function(req, res, next) {
+
+  const user = await getUser(req.params.id).catch(console.dir);
+
+  res.send(user);
+  
+});
+
+/**
+ * Get the list of users
+ */
 async function getUsers() {
   const client = new MongoClient(MONGO_URI);
   
@@ -37,8 +50,31 @@ async function getUsers() {
   } finally {
     await client.close();
   }
+}
 
+/**
+ * Get a user by id
+ * @param {string} id The internal user id 
+ */
+async function getUser(id) {
+  const client = new MongoClient(MONGO_URI);
+  
+  try {
 
+    await client.connect();
+
+    const database = client.db(MONGO_DB);
+    const collection = database.collection(MONGO_COLLECTION);
+    
+    const query = { "_id":id };
+    
+    const user = await collection.findOne(query);
+
+    return user;
+
+  } finally {
+    await client.close();
+  }
 }
 
 
